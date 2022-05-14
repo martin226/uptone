407eff8f38da7991be8754b514fd8b5db8659d6d
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-14 15:26:09 -0400

    test: remove model prediction case

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -17,11 +17,3 @@ def test_twitter_preprocess():
         'This is a tweet. #testing @martin') == 'tweet testing'
     assert twitter_preprocess(
         'Check this link out https://www.google.com') == 'check link'
-
-
-def test_make_predictions():
-    from api.predict import make_predictions
-    tweets = ['This is a tweet. #testing @martin',
-              'fuck everyone who supports human rights', 'what a bitch']
-    predictions = make_predictions(tweets)
-    assert predictions == [2, 0, 1]
