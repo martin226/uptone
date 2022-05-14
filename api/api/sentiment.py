061cb41d39f29de59f69ec25fee41aa2a5c16dd8
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-14 13:07:09 -0400

    feat: sentiment analysis

---
+++ api/api/sentiment.py
@@ -0,0 +1,9 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+sia = SentimentIntensityAnalyzer()
+
+def is_positive(text: str) -> bool:
+    """
+    Returns True if the text is positive, False otherwise.
+    """
+    return sia.polarity_scores(text)['compound'] > 0
