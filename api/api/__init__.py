b57341a85b6ed2139adefc00e0d03d3d8a4968b7
xcfrg <30675315+xcfrg@users.noreply.github.com>
2022-05-15 01:30:44 -0400

    fix: use cors
    Signed-off-by: xcfrg <30675315+xcfrg@users.noreply.github.com>

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -2,9 +2,10 @@ __version__ = "0.1.0"
 
 from api.predict import make_predictions
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/predict", methods=["POST"])
 def predict():
