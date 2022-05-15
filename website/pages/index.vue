bb173bd98f42d30b4ee69001351517d75f508a2f
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-15 02:04:30 -0400

    feat: index page

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="bg-secondary">
+    <Hero class="min-h-screen flex-1 border-b-1 border-black" />
+    <Mission class="py-16" />
+    <How class="py-16" />
+    <About class="py-16" />
+    <Footer class="py-8" />
+  </div>
 </template>
 
 <script lang="ts">
