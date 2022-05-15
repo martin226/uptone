3c37834015523ac1c0604f510bcb2ed72c620492
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-05-15 03:14:33 -0400

    fix: lint

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-unassigned-import
 import "./options-storage.js";
 import browser from "webextension-polyfill";
 
--- extension/src/content.js
+++ extension/src/content.js
@@ -1,7 +1,31 @@
 import optionsStorage from "./options-storage.js";
 import browser from "webextension-polyfill";
 
+const tweets = new Set();
+let id = 0;
+
+const createWarning = (elID) => `
+<div id="${elID}-warning" style="display: flex;background: rgb(22, 24, 28);padding: 1rem;border-radius: 16px;justify-content: space-between;">
+  <div style="color: rgb(113, 118, 123);font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif;">
+    The following tweet was hidden by Uptone due to offensive content. </div>
+  <div id="${elID}-btn" style="color: rgb(239, 243, 244);font-weight: 700;font-size: 15px;font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif;">View</div>
+</div>`;
+
+let finished = true;
 let options;
+
+const predict = async (tweetData) => {
+  return browser.runtime
+    .sendMessage({
+      method: "POST",
+      action: "xhttp",
+      url: "http://localhost:5000/predict",
+      data: JSON.stringify({ tweets: tweetData }),
+    })
+    .then((message) => {
+      return JSON.parse(message).predictions;
+    });
+};
 
 const filterTweets = async () => {
   if (!finished) return;
@@ -37,7 +61,7 @@ const filterTweets = async () => {
     if (prediction === 2) continue;
     if (!options.offensiveLanguage && prediction === 1) continue;
     if (!options.hateSpeech && prediction === 0) continue;
-    elID = `uptone-${id++}`;
+    const elID = `uptone-${id++}`;
 
     if (
       tweet.firstChild.firstChild.firstChild &&
@@ -47,7 +71,6 @@ const filterTweets = async () => {
       contentArea.childNodes[2].style.display = "none";
       contentArea.childNodes[2].id = `${elID}-content`;
       contentArea.innerHTML += createWarning(elID);
-      console.log(elID);
     } else if (
       tweet.firstChild.firstChild.firstChild.childNodes[1].childNodes[1] &&
       tweet.firstChild.firstChild.firstChild.childNodes[1].childNodes[1]
@@ -58,7 +81,6 @@ const filterTweets = async () => {
       contentArea.childNodes[1].style.display = "none";
       contentArea.childNodes[1].id = `${elID}-content`;
       contentArea.innerHTML += createWarning(elID);
-      console.log(elID);
     }
     const button = document.getElementById(`${elID}-btn`);
     if (button) {
@@ -83,31 +105,6 @@ async function init() {
 
 init();
 
-const tweets = new Set();
-let id = 0;
-
-const createWarning = (elID) => `
-<div id="${elID}-warning" style="display: flex;background: rgb(22, 24, 28);padding: 1rem;border-radius: 16px;justify-content: space-between;">
-  <div style="color: rgb(113, 118, 123);font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif;">
-    The following tweet was hidden by Uptone due to offensive content. </div>
-  <div id="${elID}-btn" style="color: rgb(239, 243, 244);font-weight: 700;font-size: 15px;font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif;">View</div>
-</div>`;
-
-let finished = true;
-
 document.addEventListener("scroll", async () => {
   await filterTweets();
 });
-
-const predict = async (tweetData) => {
-  return browser.runtime
-    .sendMessage({
-      method: "POST",
-      action: "xhttp",
-      url: "http://localhost:5000/predict",
-      data: JSON.stringify({ tweets: tweetData }),
-    })
-    .then((message) => {
-      return JSON.parse(message).predictions;
-    });
-};
